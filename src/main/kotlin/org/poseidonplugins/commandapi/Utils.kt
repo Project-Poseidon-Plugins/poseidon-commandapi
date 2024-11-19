6d9da2f76236f11f7ee475f5edb3537bcd878b0f
zavdav <157126752+zavdav@users.noreply.github.com>
2024-11-20 00:32:21 +0100

    Add getPluginCommands method

--- src/main/kotlin/org/poseidonplugins/commandapi/Utils.kt
+++ src/main/kotlin/org/poseidonplugins/commandapi/Utils.kt
@@ -2,7 +2,11 @@
 
 package org.poseidonplugins.commandapi
 
+import org.bukkit.Bukkit
+import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.command.PluginCommand
+import org.bukkit.command.SimpleCommandMap
 
 fun hasPermission(sender: CommandSender, permission: String): Boolean =
     sender.isOp || sender.hasPermission(permission) || permission == ""
@@ -16,4 +20,12 @@ fun getField(obj: Any, name: String): Any {
     val objField = field.get(obj)
     field.isAccessible = false
     return objField
+}
+
+fun getPluginCommands(): Map<String, Command> {
+    var commands = getField(getField(Bukkit.getPluginManager(), "commandMap") as SimpleCommandMap, "knownCommands") as Map<String, Command>
+
+    return commands
+        .filter { (name, command) -> command is PluginCommand && name == command.name }
+        .toSortedMap(compareBy<String> { (commands[it] as PluginCommand).plugin.description.name }.thenBy { it })
 }
