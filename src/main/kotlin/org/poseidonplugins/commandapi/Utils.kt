0b4382056db6f0a13b978aee0afff2c762072a9a
zavdav <157126752+zavdav@users.noreply.github.com>
2024-11-20 09:52:56 +0100

    Add colorize method

--- src/main/kotlin/org/poseidonplugins/commandapi/Utils.kt
+++ src/main/kotlin/org/poseidonplugins/commandapi/Utils.kt
@@ -11,8 +11,9 @@ import org.bukkit.command.SimpleCommandMap
 fun hasPermission(sender: CommandSender, permission: String): Boolean =
     sender.isOp || sender.hasPermission(permission) || permission == ""
 
-fun sendMessage(sender: CommandSender, message: String) =
-    sender.sendMessage(message.replace("&([0-9a-f])".toRegex(), "§$1"))
+fun sendMessage(sender: CommandSender, message: String) = sender.sendMessage(colorize(message))
+
+fun colorize(message: String) = message.replace("&([0-9a-f])".toRegex(), "§$1")
 
 fun getField(obj: Any, name: String): Any {
     val field = obj.javaClass.getDeclaredField(name)
